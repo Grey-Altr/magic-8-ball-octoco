b595bace155bcf1df9465c26e9210f715df02b0c
Grey Altr <grey.sifr@gmail.com>
2022-10-27 16:20:15 -0700

    added getters for ball-answer and toy

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 /* Get DOM Elements */
 const questionSection = document.getElementById('question-section');
 const submitButton = document.getElementById('submit-button');
+const ballAnswer = document.getElementById('ball-answer');
+const toy = document.getElementById('toy');
 /* State */
 
 /* Events */
@@ -10,3 +12,8 @@ const submitButton = document.getElementById('submit-button');
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
+// js below for ball from https://codepen.io/finnhvman/pen/wrLPJz
+function validateBall() {
+    const randomId = Math.floor(Math.random() * 20);
+    document.getElementById(randomId).checked = true;
+}
